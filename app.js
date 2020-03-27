b16443d451f271cf939814388f8df652ba07616a
Keenan Gaudio <35779579+keenangaudio@users.noreply.github.com>
2020-03-26 18:47:22 -0600

    Update app.js

--- app.js
+++ app.js
@@ -9,19 +9,12 @@ $(document).ready(function () {
     }
 });
 
-fizzbuzz = (val) => {
+const fizzbuzz = (val) => {
     for (let i = 1; i <= val; i++) {
-        if (i % 3 == 0 && i % 5 == 0) {
-            $("body").append("fizzbuzz<br>");
-        }
-        else if (!(i % 5)) {
-            $("body").append("buzz<br>");
-        }
-        else if (!(i % 3)) {
-            $("body").append("fizz<br>");
-        }
-        else {
-            $("body").append(i + '<br>');
-        }
+        let str = "";
+        if (!(i % 3)) str = "fizz";
+        if (!(i % 5)) str += "buzz";
+ 
+        $("body").append((str || i) + '<br>');
     }
 }
